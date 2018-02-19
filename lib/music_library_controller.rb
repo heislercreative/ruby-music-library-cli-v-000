22efaf1daea8a763ddd0d9f45001bd115b0845fe
Brennan Heisler <learn@heislercreative.com>
2018-02-19 03:40:06 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -19,7 +19,10 @@ class MusicLibraryController
           list_artists
         when "list genres"
           list_genres
-        when
+        when "list artist"
+          list_songs_by_artist
+        when "list genre"
+          list_songs_by_genre
       end
     end
   end
