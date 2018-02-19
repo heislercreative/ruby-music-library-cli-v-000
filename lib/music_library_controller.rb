4ae80a937887a70a4424ff2ed5c6378a78a2a445
Brennan Heisler <learn@heislercreative.com>
2018-02-19 03:55:52 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -47,7 +47,7 @@ class MusicLibraryController
       a.name <=> b.name
     end.each.with_index(1) do |song, index|
       puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
-      end
+    end
   end
   
   def list_artists
