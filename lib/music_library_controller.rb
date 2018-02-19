bb58381ffffa9a13511856c15b5984bbbe1d55eb
Brennan Heisler <learn@heislercreative.com>
2018-02-19 03:34:10 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -28,4 +28,30 @@ class MusicLibraryController
     puts "What would you like to do?"
   end
   
+  def list_songs
+    
+  end
+  
+  def list_artists
+    
+  end
+  
+  def list_genres
+    
+  end
+  
+  def list_songs_by_artist
+    
+  end
+  
+  def list_songs_by_genre
+    
+  end
+  
+  def play_song
+    
+  end
+  
+  
+  
 end
