ac30925b4bce79455f3858056d75aabd9a1053e3
Brennan Heisler <learn@heislercreative.com>
2018-02-19 04:01:45 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -70,6 +70,11 @@ class MusicLibraryController
     puts "Please enter the name of an artist:"
     input = gets.strip
     
+    input.songs.sort do |a, b|
+      a.name <=> b.name
+    end.each.with_index(1) do |song, index|
+      puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+    end
   end
   
   def list_songs_by_genre
